58f333653dda9638229b89e1c1a3b44b95f13016
CyrilJay69 <144930694+CyrilJay69@users.noreply.github.com>
2023-09-13 23:24:08 +0800

    Add files via upload

---
+++ SavingsAccount.cs
@@ -0,0 +1,13 @@
+public abstract class SavingsAccount : Account{
+        public double Interestrate {get;}
+        public SavingsAccount(int Accountnumber, double Interestrate) : base (Accountnumber)
+        {
+            this.Interestrate=Interestrate;
+        }
+        public void CalculatedInterest(){
+             double interest = Balance*(Interestrate/100) ;
+             double interest1 = interest+Balance;
+             Balance=interest1;
+             Console.WriteLine($"Interest Earned: {interest}");
+        }
+    }
